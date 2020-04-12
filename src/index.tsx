586ffca541bfe10863906f87a0788c3b51c507f5
Nick Nance <nance.nick@gmail.com>
2020-04-11 17:40:01 -0700

    working build with babel

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const Main: React.FC = () => <div>Hello World!</div>;
+
+ReactDOM.render(<Main/>, document.querySelector("#root"));
